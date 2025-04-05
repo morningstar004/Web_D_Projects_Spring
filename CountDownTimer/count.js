7fcbb78f144bcfd6073a83884d3df8564a99c77e
morningstar004 <pranjalkumar000007@gmail.com>
2025-04-05 12:56:43 +0530

    Added funtions ton Quick timer buttons and fixed total time div.

--- CountDownTimer/count.js
+++ CountDownTimer/count.js
@@ -72,3 +72,87 @@ playButton.addEventListener("click", function(){
 });
 
 updateDisplay();
+
+
+const tenSecButton = document.getElementById("tenSec");
+const tenMinButton = document.getElementById("tenMin");
+const sixHourButton = document.getElementById("sixHour");
+const endbutton = document.getElementById("end");
+
+tenSecButton.addEventListener("click",function(){
+    hours = 0;
+    minutes = 0;
+    seconds = 10;
+
+    updateDisplay();
+    updateDisplayTimer()
+
+    clearInterval(timer);
+    timer = setInterval(timerUpdate,1000);
+    isRunning = true;
+
+    playIcon.classList.replace("fa-play", "fa-pause");
+    circle.classList.add("bordered");
+
+})
+
+tenMinButton.addEventListener("click",function(){
+    hours = 0;
+    minutes = 10;
+    seconds = 0;
+
+    updateDisplay();
+    updateDisplayTimer()
+
+    clearInterval(timer);
+    timer = setInterval(timerUpdate,1000);
+    isRunning = true;
+
+    playIcon.classList.replace("fa-play", "fa-pause");
+    circle.classList.add("bordered");
+
+})
+
+sixHourButton.addEventListener("click",function(){
+    hours = 6;
+    minutes = 0;
+    seconds = 0;
+
+    updateDisplay();
+    updateDisplayTimer()
+
+    clearInterval(timer);
+    timer = setInterval(timerUpdate,1000);
+    isRunning = true;
+
+    playIcon.classList.replace("fa-play", "fa-pause");
+    circle.classList.add("bordered");
+
+})
+
+endbutton.addEventListener("click",function(){
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+
+    updateDisplay();
+    updateDisplayTimer()
+
+    clearInterval(timer);
+    timer = setInterval(timerUpdate,1000);
+    isRunning = false;
+
+    playIcon.classList.replace("fa-pause", "fa-play");
+    circle.classList.remove("bordered");
+
+})
+
+const totalHour = document.getElementById("totalHour");
+const totalMin = document.getElementById("totalMin");
+const totalSec = document.getElementById("totalSec");
+
+function updateDisplayTimer(){
+    totalHour.textContent = `${String(hours).padStart(2, "0")} hour${hours !== 1 ? "s" : ""}`;
+    totalMin.textContent = `${String(minutes).padStart(2, "0")} minute${minutes !== 1 ? "s" : ""}`;
+    totalSec.textContent = `${String(seconds).padStart(2, "0")} second${seconds !== 1 ? "s" : ""}`;
+}
